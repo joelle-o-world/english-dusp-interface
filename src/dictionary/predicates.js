c987d670a1362bfee0d7f30a897377f6c73c1ba6
Joel Plowright <joelplow@gmail.com>
2019-04-12 19:45:43 +0100

    basic dictionary

--- src/dictionary/predicates.js
+++ src/dictionary/predicates.js
@@ -1,13 +1,27 @@
 const {Predicate} = require('english-io')
 
 const BeAnInputOf = new Predicate({
-  verb: 'be an input', params:['subject', 'of']
+  verb: 'be an input', params:['subject', 'of'],
+
+  problem(inlet) {
+    return !inlet.is_a('input')
+  }
 })
+
 const BeAnOutputOf = new Predicate({
   verb: 'be an output', params:['subject', 'of'],
+
+  problem(outlet) {
+    return !outlet.is_a('output')
+  },
 })
+
 const BeRoutedTo = new Predicate({
-  verb: 'be routed', params:['subject', 'to']
+  verb: 'be routed', params:['subject', 'to'],
+
+  problem(inlet, outlet) {
+    return !(inlet.is_a('input') && outlet.is_a('output'))
+  },
 })
 
 module.exports = {
--- test/test_dictionary.js
+++ test/test_dictionary.js
@@ -1,7 +1,14 @@
+const {WanderingDescriber} = require('english-io')
+
 const d = require('../src/dictionary')
 
 let entities = d.quickDeclare(
-  'the input is routed to the output'
+  'the input is routed to the output',
+  'the input is an input of the unit',
+  'the output is an output of another unit'
 )
 
-console.log(entities)
+let describer = new WanderingDescriber(...entities)
+let next
+while((next = describer.next()))
+  console.log(next.str())
